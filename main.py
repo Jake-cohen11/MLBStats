14f68955c1a3f0d74718961d2b9f3b107a7ac553
Jake Cohen <jcohen0121@gmail.com>
2024-10-14 16:39:06 -0400

    added pull for mlb stats

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-#test
-#test2
-#test3
+from pybaseball import batting_stats
+import pandas
+import csv
+from functools import reduce
+
+data = batting_stats(2024, qual=50)
+
+ignore = ["IDfg", "Season", "Age", "Dol", "Age Rng", "G", "AB", "PA"]
+data.drop(ignore, axis=1, inplace=True)
+
+print(data)
